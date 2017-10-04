b08508b02cfea8941bd5dd7d7c26c70d2fd8476d
Brent M. Spell <bspell1@gmail.com>
2017-10-04 17:58:47 -0400

    add credo, dialyzer, nif compile

--- mix.exs
+++ mix.exs
@@ -6,23 +6,32 @@ defmodule NlPotion.Mixfile do
       app: :nl_potion,
       version: "0.1.0",
       elixir: "~> 1.5",
+      compilers: [:nif] ++ Mix.compilers,
       start_permanent: Mix.env == :prod,
-      deps: deps()
+      deps: deps(),
+      dialyzer: [ignore_warnings: ".dialyzerignore",
+                 plt_add_deps: :transitive]
     ]
   end
 
-  # Run "mix help compile.app" to learn about applications.
   def application do
     [
       extra_applications: [:logger]
     ]
   end
 
-  # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
+      {:credo, "~> 0.3", only: [:dev, :test]},
+      {:dialyxir, "~> 0.5", only: [:dev], runtime: false},
+      {:xxhash, "~> 0.2.0", hex: :erlang_xxhash}
     ]
+  end
+end
+
+defmodule Mix.Tasks.Compile.Nif do
+  def run(_args) do
+    {result, _errcode} = System.cmd("make", ["-s", "-C", "c_src"])
+    IO.binwrite(result)
   end
 end
