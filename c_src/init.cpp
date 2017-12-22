bf7f1ff1927c4b4daaa71f3f711888e3243558bd
Brent M. Spell <bspell1@gmail.com>
2017-12-22 13:57:59 -0500

    initial liblinear nifs

--- c_src/init.cpp
+++ c_src/init.cpp
@@ -16,12 +16,18 @@
 /*-------------------[        Global Variables         ]-------------------*/
 /*-------------------[        Global Prototypes        ]-------------------*/
 extern int nif_blas_init (ErlNifEnv* env);
+extern int nif_lin_init  (ErlNifEnv* env);
 extern int nif_svm_init  (ErlNifEnv* env);
 extern int nif_crf_init  (ErlNifEnv* env);
 /*-------------------[        Module Variables         ]-------------------*/
 /*-------------------[        Module Prototypes        ]-------------------*/
 DECLARE_NIF(blas_sscal);
 DECLARE_NIF(blas_saxpy);
+DECLARE_NIF(lin_train);
+DECLARE_NIF(lin_export);
+DECLARE_NIF(lin_compile);
+DECLARE_NIF(lin_predict_class);
+DECLARE_NIF(lin_predict_probability);
 DECLARE_NIF(svm_train);
 DECLARE_NIF(svm_export);
 DECLARE_NIF(svm_compile);
@@ -36,6 +42,11 @@ DECLARE_NIF(crf_predict);
 static ErlNifFunc nif_map[] = {
    EXPORT_NIF(blas_sscal, 2),
    EXPORT_NIF(blas_saxpy, 3),
+   EXPORT_NIF(lin_train, 3, ERL_NIF_DIRTY_JOB_CPU_BOUND),
+   EXPORT_NIF(lin_export, 1),
+   EXPORT_NIF(lin_compile, 1),
+   EXPORT_NIF(lin_predict_class, 2),
+   EXPORT_NIF(lin_predict_probability, 2),
    EXPORT_NIF(svm_train, 3, ERL_NIF_DIRTY_JOB_CPU_BOUND),
    EXPORT_NIF(svm_export, 1),
    EXPORT_NIF(svm_compile, 1),
@@ -59,10 +70,12 @@ static int nif_loaded (ErlNifEnv* env, void** priv_data, ERL_NIF_TERM state)
    *priv_data = NULL;
    if (!nif_blas_init(env))
       return 1;
+   if (!nif_lin_init(env))
+      return 2;
    if (!nif_svm_init(env))
-      return 2;
+      return 3;
    if (!nif_crf_init(env))
-      return 2;
+      return 4;
    return 0;
 }
 // nif entry point
