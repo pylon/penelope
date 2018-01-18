4fdb115395362eb28503dc869bb6d57a9f49fd10
Brent M. Spell <bspell1@gmail.com>
2018-01-18 11:51:48 -0500

    release 0.3.1

--- README.md
+++ README.md
@@ -29,7 +29,7 @@ sudo apt install libblas-dev
 ```elixir
 def deps do
   [
-    {:penelope, "~> 0.3.0"}
+    {:penelope, "~> 0.3"}
   ]
 end
 ```
--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Penelope.Mixfile do
     [
       app:               :penelope,
       name:              "Penelope",
-      version:           "0.3.0",
+      version:           "0.3.1",
       elixir:            "~> 1.6",
       compilers:         ["nif" | Mix.compilers],
       aliases:           [clean: ["clean", "clean.nif"]],
